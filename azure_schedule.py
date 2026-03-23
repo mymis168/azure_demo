f159ab2c798123a8fc9da8d32edc6f1cdd8ec29f
michael <mymis168@gmail.com>
2026-03-23 11:33:05 +0800

    initial commit

---
+++ azure_schedule.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import time
+import schedule
+
+def sayHello():
+    now = datetime.now()
+    print(f'固定每十秒鐘 {now}')
+
+
+schedule.every(10).seconds.do(sayHello).unit("12:00")
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+    
